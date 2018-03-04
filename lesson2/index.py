75c488b6ec7c9c30929ec715eb3e8ef317f1da35
karim140195 <keko.assy@yahoo.com>
2018-03-04 23:43:18 +0200

    update rename func

--- lesson2/index.py
+++ lesson2/index.py
@@ -1,12 +1,18 @@
 import os
 
-# this function print all names of files at this directory
+# this function print all names of files  this directory
 
 
 def rename_files():
     fil_list = os.listdir(
         r"G:\udacity nano degree\Programming-Foundations-with-Python\lesson2\files")
     print(fil_list)
+    saved_path = os.getcwd()
+    os.chdir(
+        r"G:\udacity nano degree\Programming-Foundations-with-Python\lesson2\files")
+    for file_name in fil_list:
+        os.rename(file_name, file_name.strip("0123456789"))
+    os.chdir(saved_path)
 
 
 rename_files()
