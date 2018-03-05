bf3d69c94889a94839de680da4815880a2116db4
karim140195 <keko.assy@yahoo.com>
2018-03-05 19:09:15 +0200

    lesson 4

---
+++ lesson4/code1/index.py
@@ -0,0 +1,12 @@
+# Sending an SMS using the Twilio API
+from twilio import rest
+# put your own credentials here
+account_sid = "ACf8c21d1fd4a593deb2a2d2b45367ec87"
+auth_token = "2a7b427096afb43d5d7d6aea03fdcec1"
+client = rest.Client(account_sid, auth_token)
+message = client.messages.create(
+    to="+2011111111111",  # vertified phone number from trello
+    from_="+14158623509",  # phone number recieved by trello
+    body="Tomorrow's forecast in Financial District, San Francisco is Clear",
+    media_url="https://climacons.herokuapp.com/clear.png")
+print(message.sid)
